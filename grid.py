ed96e632b227ddda28dde816606dbf3acc7d3318
Michał El <m.elfartas@gmail.com>
2015-08-31 23:14:20 +0200

    Subscribers selection algorithm.

--- grid.py
+++ grid.py
@@ -1,6 +1,7 @@
 __author__ = 'micmax93'
 
 import api
+from datetime import datetime
 
 child = api._age_child
 young = api._age_young
@@ -20,6 +21,9 @@ class AgeGenderGrid(object):
 
     def __init__(self, value=None):
         self.table = [[value for x in range(2)] for x in range(4)]
+
+    def clear(self, value=None):
+        self.__init__(value)
 
     def _set_col(self, row, gender, cb):
         gender = self.col_dict[gender]
@@ -67,6 +71,56 @@ class SubscribersGrid(AgeGenderGrid):
         self.set(age, gender, cb=self._get_cb(subscriber))
 
 
+class SubscribersList():
+    def __init__(self, subscribers_grid):
+        self.subscribers = {}
+        assert isinstance(subscribers_grid, SubscribersGrid)
+        self.grid = subscribers_grid.table
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[i])):
+                for k in range(len(self.grid[i][j])):
+                    self.add(self.grid[i][j][k])
+
+    def add(self, subscriber):
+        if subscriber not in self.subscribers:
+            self.subscribers[subscriber] = {'sub': subscriber, 'viewers': 0, 'selected': False, 'date': datetime.now()}
+
+    def clear_viewings(self):
+        for s in self.subscribers:
+            self.subscribers[s]['viewers'] = 0
+
+    def clear_selection(self):
+        for s in self.subscribers:
+            self.subscribers[s]['selections'] = False
+
+    def update_viewers(self, viewers_grid):
+        self.clear_viewings()
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[i])):
+                for k in range(len(self.grid[i][j])):
+                    self.subscribers[self.grid[i][j][k]] += viewers_grid.table[i][j]
+
+    def choose_subscriber(self, targeted_alg=True):
+        subs = filter(lambda s: not s['selected'], self.subscribers.values())
+        if len(subs) == 0:
+            self.clear_selection()
+            subs = self.subscribers.values()
+        val = max(s['viewers'] for s in subs)
+        if val == 0 and targeted_alg:
+            self.clear_selection()
+            subs = self.subscribers.values()
+            val = max(s['viewers'] for s in subs)
+        subs = filter(lambda s: s['viewers'] == val, subs)
+        subs = sorted(subs, key=lambda s: s['date'])
+        return subs[0]['sub']
+
+    def select_subscriber(self, viewers_grid, targeted_alg=True):
+        assert isinstance(viewers_grid, ViewersGrid)
+        self.update_viewers(viewers_grid)
+        sub = self.choose_subscriber(targeted_alg)
+        self.subscribers[sub]['selected'] = True
+        self.subscribers[sub]['date'] = datetime.now()
+        return sub
 
 
 
