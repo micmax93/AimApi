b7fce626f74a7490db7d454e14aa829ad77604c2
Michał El <m.elfartas@gmail.com>
2015-09-03 22:57:23 +0200

    Connecting all modules together.

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
 __author__ = 'micmax93'
 from api import ApiConnection
 from player import SinglePlayer
+from loader import load_configs
+from grid import SubscribersList, ViewersGrid
+from ConfigParser import ConfigParser
 
 
-api = ApiConnection(host='25.152.172.38')
+_config = ConfigParser()
+_config.read('config.ini')
+_path = _config.get('global', 'path')
+_mode = _config.getboolean('global', 'targeted')
+
+
+subscribers, _grid = load_configs(_path)
+sub_list = SubscribersList(_grid)
+
 player = SinglePlayer()
 
-while True:
-    viewers = api.get_audience_details()
-    # TODO
-    player.set_next('path')
+api = ApiConnection(host='25.152.172.38')
+
+
+def get_viewers_grid(viewers):
+    grid = ViewersGrid()
+    for viewer in viewers:
+        grid.add(viewer.age, viewer.gender)
+    return grid
+
+
+def run_once():
+    audience = api.get_audience_details()
+    viewers_grid = get_viewers_grid(audience)
+    sub = sub_list.select_subscriber(viewers_grid, targeted_alg=_mode)
+    player.set_next(subscribers[sub]['video'])
     player.wait()
 
 
+if __name__ == '__main__':
+    while True:
+        run_once()
